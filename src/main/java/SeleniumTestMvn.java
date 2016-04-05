0441068bd3e0605bb9497fc4683a1a7a31914f88
Sony <hbhbhj@sdfdfs.ru>
2016-04-05 20:29:39 +0300

    njnnj

--- src/main/java/SeleniumTestMvn.java
+++ src/main/java/SeleniumTestMvn.java
@@ -1,27 +1,44 @@
 import junit.framework.TestCase;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTestMvn extends TestCase {
     private WebDriver driver = new FirefoxDriver();
     private By input = By.id("text");
-                
+
     @BeforeClass
     public void setUp() throws Exception {
         this.driver.get("https://ya.ru");
     }
+    @Test
     public void test() throws Exception {
         //System.out.println(this.driver.getCurrentUrl());
         WebElement yaSearchInput = driver.findElement(input);
+        assertTrue(yaSearchInput.isDisplayed());
+
         yaSearchInput.sendKeys("Woof");
         yaSearchInput.submit();
         System.out.println("Page title is: " +driver.getTitle());
-        assertEquals("Woof");
+        //WebDriverWait wait = new WebDriverWait(driver, 15);
+
+        //assertEquals("Wrong page",driver.getTitle().contains("Woof"));
+
     }
+
+    public void testSecond() throws Exception {
+        //System.out.println(this.driver.getCurrentUrl());
+        WebElement yaSearchInput = driver.findElement(input);
+        WebElement imgLink = driver.findElement()
+        assertTrue(yaSearchInput.isDisplayed());
+    }
+
 
     @AfterClass
     public void tearDown() throws Exception {
